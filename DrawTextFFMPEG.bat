9a40234dc4ee8b56aa672bbee5668f4b290b3928
Ionut Dincu <gabrieldincu@outlook.com>
2024-07-16 23:18:29 +0300

    Update DrawTextFFMPEG.bat

--- DrawTextFFMPEG.bat
+++ DrawTextFFMPEG.bat
@@ -1,1 +1,27 @@
-ffmpeg -hide_banner -loglevel error -y -i final_result_4K.mp4 -vf "drawtext=fontfile=C\\:/Windows/Fonts/RockwellNova-ExtraBold.ttf:text='evDashVisualizer 4K':x=(w-text_w)/2:y=(h-text_h)/2:enable='between(t,1,6)':fontsize=240:fontcolor=black:box=1:boxborderw=33:boxcolor=white@0.25" -codec:a copy output.mp4
+@echo off
+setlocal enabledelayedexpansion
+
+REM Clearing the screen
+test&cls
+
+echo Adding custom text to the mp4 file...
+
+REM Constructing the comand using a string builder
+set "cmd=ffmpeg -hide_banner -loglevel error -y -i final_result_2K.mp4 -vf "
+
+REM Drawtext #1
+set "cmd=!cmd!"drawtext=fontfile=C\\:/Windows/Fonts/RockwellNova-ExtraBold.ttf:text='TEXT1':x=(w-text_w)/2:y=(h-text_h)/2:enable='between(t,1,3)':fontsize=240:fontcolor=black:box=1:boxborderw=33:boxcolor=white@0.25,"
+
+REM Drawtext #2
+set "cmd=!cmd!drawtext=fontfile=C\\:/Windows/Fonts/RockwellNova-ExtraBold.ttf:text='TEXT2':x=(w-text_w)/2:y=(h-text_h)/2:enable='between(t,4,6)':fontsize=240:fontcolor=black:box=1:boxborderw=33:boxcolor=white@0.25""
+
+REM Specifying an output file
+set "cmd=!cmd! -codec:a copy output.mp4"
+
+:: Execute the constructed command
+!cmd!
+
+echo [92mmp4 file updated successfully[0m
+echo.
+
+pause
